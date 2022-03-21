7c722829783b629ac7f6af3700cfc82258ea308e
Perexd <91455233+Perexd@users.noreply.github.com>
2022-03-21 13:57:41 +0100

    hola

--- Assets/scrips/playercontroler.cs
+++ Assets/scrips/playercontroler.cs
@@ -3,43 +3,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class playercontroler : MonoBehaviour
 {
-   private float movimiento;
-   private float rotacion;
-public float movementspeed =20f;
-public float rotationspeed =80f;
- private int lives = 3;
- private int end = 0;
-  public bool gameOver;
-  public bool Win;
+    private float movimiento;
+    private float rotacion;
+    public float movementspeed = 20f;
+    public float rotationspeed = 80f;
+    private int lives = 3;
+    private int end = 0;
+    public GameObject gameOver;
+    public GameObject Win;
+    public GameObject vidasxd;
+    public TextMeshProUGUI Texto;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        gameOver.SetActive(false);
+        Win.SetActive(false);
+        vidasxd.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-     movimiento = Input.GetAxis("Vertical");   
-     rotacion = Input.GetAxis("Horizontal");   
+        movimiento = Input.GetAxis("Vertical");
+        rotacion = Input.GetAxis("Horizontal");
 
-     transform.Translate(Vector3.forward * movementspeed * Time.deltaTime * movimiento);
-        
-     transform.Rotate(Vector3.up, rotationspeed * Time.deltaTime * rotacion);
+        transform.Translate(Vector3.forward * movementspeed * Time.deltaTime * movimiento);
+
+        transform.Rotate(Vector3.up, rotationspeed * Time.deltaTime * rotacion);
+
+       Texto.text = $"{lives}";
+    }
 
 
-    }  
-  
-     
     private void OnTriggerEnter(Collider otherCollider)
     {
         if (otherCollider.gameObject.CompareTag("Powerup"))
         {
-           
+
 
             lives++;
 
@@ -51,27 +57,28 @@ public float rotationspeed =80f;
                 win();
             }
 
-         Destroy(otherCollider.gameObject);
+            Destroy(otherCollider.gameObject);
+
         }
-         if (otherCollider.gameObject.CompareTag("misilenemigo"))
-            {  
-                lives--;
-                if (lives <= 0)
-                {
-                    GameOver();
-                }
-                Destroy(otherCollider.gameObject);
-            } 
+        if (otherCollider.gameObject.CompareTag("misilenemigo"))
+        {
+            lives--;
+            if (lives <= 0)
+            {
+                GameOver();
+            }
+            Destroy(otherCollider.gameObject);
+        }
     }
- private void GameOver()
-    {       
-        gameOver = true;
-         Debug.Log("game over");
+    private void GameOver()
+    {
+        gameOver.SetActive(true);
+        vidasxd.SetActive(false);
     }
-private void win()
-{
-    Win = true;
-      Debug.Log("ganar");
-}
+    private void win()
+    {
+        Win.SetActive(true);
+        vidasxd.SetActive(false);
+    }
 
 }
