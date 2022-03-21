78a1ce169dc4e56bf85b6eaa6cef7e5ff5ee3dd6
Perexd <pereestade@gmail.com>
2022-03-21 14:59:14 +0100

    s

--- Assets/scrips/OVEJA.cs
+++ Assets/scrips/OVEJA.cs
@@ -8,7 +8,7 @@ public class OVEJA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
 }
 
     // Update is called once per frame
